774f6a97aa0252538914553b85e0dae688031f74
Борис Мальгин <boris_malgin@mail.ru>
2021-04-17 10:09:01 +0500

    Add controllers and server-side API

---
+++ NeurotestClient/NeurotestClient/Controllers/TestGeneratorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NeurotestServer.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestGeneratorController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Question> GetTest(TestConfig config)
+        {
+            return TestGenerator.CreateTest(config);
+        }
+    }
+}
