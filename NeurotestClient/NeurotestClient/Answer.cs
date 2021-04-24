1af1e69d823534b558d43446c945f4361eb73acc
Борис Мальгин <boris_malgin@mail.ru>
2021-04-24 17:38:28 +0500

    Fix string to single convertion

--- NeurotestClient/NeurotestClient/Answer.cs
+++ NeurotestClient/NeurotestClient/Answer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Diagnostics;
 
 namespace NeurotestServer
@@ -11,7 +12,7 @@ namespace NeurotestServer
         {
             Question question = Question.FromJSON(jsonAnswer.Question);
             EmotionType userInput = Utils.EmotionTypeStringToEnum(jsonAnswer.UserInput);
-            float elapsedTime = Convert.ToSingle(jsonAnswer.ElapsedTime);
+            float elapsedTime = Convert.ToSingle(jsonAnswer.ElapsedTime, new CultureInfo("en-US"));
 
             return new Answer(question, userInput, elapsedTime);
         }
